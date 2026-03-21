10df59f1d43428871a1b4ad5feb3c824f4953cc1
Jamey Wicklund <thebluwiz@icloud.com>
2026-03-21 12:40:14 -0700

    v1.0.2

--- muxm.rb
+++ muxm.rb
@@ -6,8 +6,8 @@
 class Muxm < Formula
   desc "Universal video encoder/muxer for DV, HDR10, HLG, and SDR with format profiles"
   homepage "https://github.com/TheBluWiz/MuxMaster"
-  url "https://github.com/TheBluWiz/MuxMaster/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e0fc60fcce2abfd8bd8a2db3c65a6aa75877acde1f71b67d8b7aa3edb9a09e0b"
+  url "https://github.com/TheBluWiz/MuxMaster/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "60c7b09c2881ed86468547518ccbbc21e592360d0d14d844a9cbd5cbb826f9b6"
   license :cannot_represent # MuxMaster Freeware License v1.0.1
 
   depends_on "bash"   # macOS ships bash 3.2; muxm requires 4.3+
